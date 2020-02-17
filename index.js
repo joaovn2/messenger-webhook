e78600b66e7fdf7789769ea1948b18278a6473e8
Joaovn2 <joaovn2@gmail.com>
2020-02-17 10:51:29 -0800

    initial commit

--- index.js
+++ index.js
@@ -26,9 +26,7 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-   res.redirect('/todo');
-});
+app.get('*', function(req, res)
 
 app.get('/webhook/', function(request, response) {
   if (request.query['hub.verify_token'] == FACEBOOK_VERIFY_TOKEN) {
