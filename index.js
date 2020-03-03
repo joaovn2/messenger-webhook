98ca4406bbe7b3c477da9cad7e69cb5d363cf7b7
joaovn2 <61025579+joaovn2@users.noreply.github.com>
2020-03-03 11:45:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -130,7 +130,10 @@ function handleMessage(sender_psid, received_message) {
    });
 
     callSendAPI(sender_psid, global.result);
-    request.delete({url: "http://075ad829.ngrok.io/rest/v1/login-sessions"});
+    if (callSendAPI(sender_psid, global.result)){
+      request.delete({url: "http://075ad829.ngrok.io/rest/v1/login-sessions"});
+    }
+    
     
     
   } else if (received_message.attachments) {
