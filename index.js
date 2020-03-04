c4f0905912580bb24de89ac6e17791f6551e67a8
joaovn2 <61025579+joaovn2@users.noreply.github.com>
2020-03-04 11:20:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ function handleMessage(sender_psid, received_message) {
       "text": JSON.stringify(body)
     };
     global.result = response;
-   },function teste(error,response,body,result){
+   },function teste(error,response,body,result,id){
    request.del({url: "http://075ad829.ngrok.io/rest/v1/login-sessions","sessionId":id });
    });
    });
