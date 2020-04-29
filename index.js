fa7be2161324f3484131732fbcca45519068920a
joaovn2 <61025579+joaovn2@users.noreply.github.com>
2020-04-28 22:50:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -108,9 +108,8 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     request.post({
-    url: "http://754f5654.ngrok.io/rest/v1/login-sessions",
-    "userName":"admin",
-    "password":"W@ster123"
+    url: "http://253f2a3f.ngrok.io/receber",
+    led: "on"
    }, function(error,response,body,result){
     var jey = JSON.parse(body);
     var id = jey.cookie; 
